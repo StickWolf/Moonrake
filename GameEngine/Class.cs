04abf96b6816acca8a56a24144467ee0c81ad7cd
Zachariah Benedict <ahloondeeli@gmail.com>
2019-02-17 15:07:40 -0800

    Added Class Character (and made other things without "1"'s)

--- GameEngine/Class.cs
+++ GameEngine/Class.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameData
+namespace GameEngine
 {
-    public class Class1
+    public class Class
     {
+
     }
 }
