98a2cc26129eed631af90b88dda42e972e588e08
Amaya Benedict <ahleedeeli@gmail.com>
2019-02-17 13:37:47 -0800

    something

---
+++ SoundUtils1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundUtils1
+{
+    public class Class1
+    {
+        public void AttacksBeep()
+        {
+            Console.Beep(400, 100);
+            Console.Beep(500, 100);
+            Console.Beep(600, 100);
+            Console.Beep(400, 100);
+        }
+
+        public void HealBeeps()
+        {
+            Console.Beep(800, 100);
+            Console.Beep(700, 100);
+            Console.Beep(600, 100);
+            Console.Beep(700, 100);
+        }
+
+        public void AcientPianoBeeps()
+        {
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+        }
+
+        public void AcientHarpBeeps()
+        {
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+        }
+
+        public void AcientDrumBeeps()
+        {
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+            Console.Beep(100, 100);
+        }
+    }
+}
