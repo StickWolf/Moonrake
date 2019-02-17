b8f1d0c4eacff2b3e01e3e0c8ddc105b7497ca78
XanderSuFromQBIX <47682296+XanderSuFromQBIX@users.noreply.github.com>
2019-02-17 15:13:21 -0800

    Added Class Character (and made other things without "1"'s) (#4)

--- GameData/Class.cs
+++ GameData/Class.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameEngine
+namespace GameData
 {
-    public class Class1
+    public class Class
     {
     }
 }
---
+++ GameEngine/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    class Character
+    {
+        private string Name;
+        private int Hp, FullHp;
+
+        public Character(string name, int hp)
+        {
+            Name = name;
+            Hp = hp;
+            FullHp = hp;
+        }
+    }
+}
