b7e8f25b5b5a20b0510504683e0baa0c5fbb996e
Matthew Benedict <matt@mattandwell.net>
2019-02-23 17:37:22 -0800

    Adding a super simple game state class.

---
+++ GameEngine/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    /// <summary>
+    /// The GameState class is meant to represent the current state of the game.
+    /// This class holds any data that should be saved and reloaded for saved games.
+    /// </summary>
+    public class GameState
+    {
+        public string PlayerName { get; set; }
+    }
+}
