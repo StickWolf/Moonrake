04a12ae300f371b06460ec81ebc1ea458d994d2b
Matthew Benedict <matt@mattandwell.net>
2019-03-16 19:24:16 -0700

    Establish the structure we want for connected locations and how they work

--- GameEngine/Locations/Location.cs
+++ GameEngine/Locations/Location.cs
@@ -6,11 +6,22 @@ namespace GameEngine.Locations
     {
         public string Name { get; private set; }
 
-        public List<LocationConnection> LocationConnections { get; private set; } = new List<LocationConnection>();
+        public List<Portal> Portals { get; private set; } = new List<Portal>();
 
         public Location(string locationName)
         {
             Name = locationName;
+        }
+
+        /// <summary>
+        /// Helper method that can be used to add a portal to a location more elegantly
+        /// </summary>
+        /// <param name="destinationRules">An array of portal rules to apply in order to the portal</param>
+        public void AddPortal(params PortalDestinationRule[] destinationRules)
+        {
+            var portal = new Portal();
+            portal.DestinationRules = new List<PortalDestinationRule>();
+            portal.DestinationRules.AddRange(destinationRules);
         }
     }
 }
---
+++ GameEngine/Locations/PortalDestinationRule.cs
@@ -0,0 +1,28 @@
+﻿namespace GameEngine.Locations
+{
+    /// <summary>
+    /// A portal destination rule is tested to see if a portal links to the specified destination.
+    /// A location can have multiple destination rules to determine what the actual destination is.
+    /// </summary>
+    public abstract class PortalDestinationRule
+    {
+        /// <summary>
+        /// The destination of where the portal leads to if the rule matches.
+        /// This may be null if the portal is closed.
+        /// </summary>
+        public string Destination { get; private set; }
+
+        /// <summary>
+        /// A description of what the portal looks like when the rule matches.
+        /// This description should only define what the portal portion looks like
+        /// and not the room beyond.
+        /// </summary>
+        public string Description { get; private set; }
+
+        public PortalDestinationRule(string destination, string description)
+        {
+            Destination = destination;
+            Description = description;
+        }
+    }
+}
