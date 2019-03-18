2c527fc160d90b82af122d63fec8f80dbae501a1
Materia-xx <170386+Materia-xx@users.noreply.github.com>
2019-03-17 19:17:09 -0700

    Fix a bug where portal rules weren't being added properly. (#36)
    Add remote and local descriptions to locations

--- GameEngine/Locations/Location.cs
+++ GameEngine/Locations/Location.cs
@@ -4,13 +4,38 @@ namespace GameEngine.Locations
 {
     public class Location
     {
+        /// <summary>
+        /// This is the name of the location and must be unique within all locations.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// A description of what this location looks like when the user is at the location.
+        /// </summary>
+        public string LocalDescription { get; private set; }
+
+        /// <summary>
+        /// A description off what this location looks like when the user is
+        /// looking ant the location from a remote location.
+        /// </summary>
+        public string RemoteDescription { get; private set; }
+
+        /// <summary>
+        /// A list of one-way connections that appear in this room and that
+        /// potentially lead to another room
+        /// </summary>
         public List<Portal> Portals { get; private set; } = new List<Portal>();
 
-        public Location(string locationName)
+        /// <summary>
+        /// Constructs a new Location
+        /// </summary>
+        /// <param name="remoteDescription">A description of what the location looks like from a remote location.</param>
+        /// <param name="localDescription">A description of what the location looks like when the user is in it.</param>
+        public Location(string locationName, string remoteDescription, string localDescription)
         {
             Name = locationName;
+            LocalDescription = localDescription;
+            RemoteDescription = remoteDescription;
         }
 
         /// <summary>
@@ -22,6 +47,7 @@ namespace GameEngine.Locations
             var portal = new Portal();
             portal.DestinationRules = new List<PortalDestinationRule>();
             portal.DestinationRules.AddRange(destinationRules);
+            Portals.Add(portal);
         }
     }
 }
