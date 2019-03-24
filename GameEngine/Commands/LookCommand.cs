d9cc9b8716495e5fa9aa17f019b980954db720c0
Zachariah Benedict <ahloondeeli@gmail.com>
2019-03-24 09:54:22 -0700

    WIP look command

--- GameEngine/Commands/LookCommand.cs
+++ GameEngine/Commands/LookCommand.cs
@@ -17,7 +17,7 @@ namespace GameEngine.Commands
             var location = engine.GameData.Locations.FirstOrDefault(loc => loc.Name.Equals(characterLocationName));
 
             // Display the local dicription of the location
-            Console.WriteLine(location?.LocalDescription);
+            Console.WriteLine(location?.LocalDescription.AddLineReturns(true));
 
             // Loop through all portals in that location 
             foreach(var portal in location.Portals)
@@ -27,7 +27,23 @@ namespace GameEngine.Commands
 
 
                 // - elavuate eatch rule and choose what should be diplayed 
-                Console.WriteLine(portal);
+                if(portalDest.Description == null)
+                {
+                    // The player sees nothing
+                } 
+                else if(portalDest.Destination == null)
+                {
+                    // If we get here, the Description exists, but the destination does not
+                    Console.WriteLine(portalDest.Description.AddLineReturns(true));
+                }
+                else
+                {
+                    // If we got here, the Description AND the destiation exist.
+                    var remoteLocation = engine.GameData.Locations.First(loc => loc.Name.Equals(portalDest.Destination));
+                    string discriptionAndDestination = portalDest.Description + ' ' + remoteLocation.RemoteDescription;
+                    // TODO: write a new specialized Console class that can add these line returns automatically
+                    Console.WriteLine(discriptionAndDestination.AddLineReturns(true));
+                }
                 //if portal is open then get remote dicripion
             }
         }
