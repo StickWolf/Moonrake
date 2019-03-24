ea3822ee3c9c488af4f36007d78c712dfa20cfbe
StickWolf <47696463+StickWolf@users.noreply.github.com>
2019-03-23 21:21:02 -0700

    locations and portals (#41)
    * filled in  the GameState constructor
    
    * finished filling in the GameState class
    
    * LetPlayerChangeTheirName Command
    
    * added new locations
    
    * spell check
    
    * fixes
    
    * locations and portals

--- GameEngine/Locations/PortalDestinationOpenGameVarRule.cs
+++ GameEngine/Locations/PortalDestinationOpenGameVarRule.cs
@@ -3,13 +3,13 @@
     /// <summary>
     /// This portal rule is used if the specified game variable equals the expected value
     /// </summary>
-    public class PortalDestinationGameVarRule : PortalDestinationRule
+    public class PortalDestinationOpenGameVarRule : PortalDestinationRule
     {
         public string GameVarName { get; private set; }
 
         public string ExpectedValue { get; private set; }
 
-        public PortalDestinationGameVarRule(string destination, string description, string gameVarName, string expectedValue)
+        public PortalDestinationOpenGameVarRule(string destination, string description, string gameVarName, string expectedValue)
             : base(destination, description)
         {
             GameVarName = gameVarName;
