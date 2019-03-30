28c012363c869279e6614b8e24a9cb5c95cf5b15
StickWolf <47696463+StickWolf@users.noreply.github.com>
2019-03-30 15:52:12 -0700

    made one tradeset (#55)
    * made one tradeset

---
+++ GameEngine/ItemRecipe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GameEngine
+{
+    public class ItemRecipe
+    {
+        public string ItemResult { get; private set; }
+        public Dictionary<string, int> Ingredients { get; private set; } = new Dictionary<string, int>();
+
+        public ItemRecipe(string itemResult, params ItemRecipeIngredient[] ingredients)
+        {
+            ItemResult = itemResult;
+            
+            foreach (var ingredient in ingredients)
+            {
+                Ingredients[ingredient.IngredientName] = ingredient.Amount;
+            }
+        }
+    }
+}
