09f67e14cd4eda9f126f447e7bc25d46d4378eea
Zachariah Benedict <ahloondeeli@gmail.com>
2019-04-13 11:59:01 -0700

    Finished

---
+++ DreamsAndWhatTheyMean/TheTaleOfTheDragonKittySourceData.cs
@@ -0,0 +1,62 @@
+﻿using GameEngine;
+using GameEngine.Locations;
+
+namespace DreamsAndWhatTheyMean
+{
+    class TheTaleOfTheDragonKittySourceData : GameSourceData
+    {
+        public DragonKittyCharacters Characters { get; private set; }
+        public DragonKittyLocations Locations { get; private set; }
+        public DragonKittyGameVarables GameVarables { get; private set; }
+        public DragonKittyItems Items { get; private set; }
+
+        public TheTaleOfTheDragonKittySourceData()
+        {
+            Characters = new DragonKittyCharacters(this);
+            Locations = new DragonKittyLocations(this);
+            GameVarables = new DragonKittyGameVarables(this);
+            Items = new DragonKittyItems(this);
+
+            DefaultPlayerName = "James";
+
+            GameIntroductionText = "Once, there was a group of kids." +
+                " These kids played around every day, they even had a youtube channel that they shared." +
+                " Now, in this story, you will join these kids and go on a journey with them." +
+                " Their names are: Zach, and Amaya." +
+                " Welcome to The Tale of The DragonKitty.";
+
+            #region Starter Items
+            AddDefaultCharacterItem(Characters.Player, Items.Money, 200);
+            #endregion
+
+            #region Room Items
+            AddDefaultLocationItem(Locations.PlayersRoom, Items.Money, 30);
+            AddDefaultLocationItem(Locations.PlayersRoom, Items.Paper, 100);
+            AddDefaultLocationItem(Locations.PlayersBackyard, Items.BronzeChunk, 10);
+            AddDefaultLocationItem(Locations.PlayersBackyard, Items.Money, 1);
+            AddDefaultLocationItem(Locations.BlackSmithShop, Items.BronzeBar, 3);
+            #endregion
+
+            #region Character Locations
+            AddDefaultCharacterLocation(Characters.Player, Locations.PlayersRoom);
+            AddDefaultCharacterLocation(Characters.DadCharacter, Locations.PlayersBackyard);
+            AddDefaultCharacterLocation(Characters.MomCharacter, Locations.PlayersLivingRoom);
+            AddDefaultCharacterLocation(Characters.BlackSmithCharacter, Locations.BlackSmithShop);
+            #endregion
+
+            #region Trade-Sets
+            var tsBlackSmith = AddTradeSet("Metal",
+                new ItemRecipe(Items.BronzeBar,
+                    new ItemRecipeIngredient(Items.BronzeChunk, 3),
+                    new ItemRecipeIngredient(Items.Money, 50)
+                    ));
+            #endregion
+
+            #region Trade-Posts
+            var tpBlackSmith = AddTradePost("The Black-Smith",
+                tsBlackSmith);
+            DefaultTradePostLocations[tpBlackSmith] = Locations.BlackSmithShop; 
+            #endregion
+        }
+    }
+}
