5e10a225750edcde3e1aae9c3dcec19573064c51
Zachariah Benedict <ahloondeeli@gmail.com>
2019-05-01 20:20:14 -0700

    stuff.

--- DreamsAndWhatTheyMean/DragonKittyStrangeCharacters/HealingDrone.cs
+++ DreamsAndWhatTheyMean/DragonKittyStrangeCharacters/HealingDrone.cs
@@ -11,7 +11,7 @@ namespace DreamsAndWhatTheyMean.DragonKittyStrangeCharacters
 {
     class HealingDrone : Character
     {
-        public Location MainLocation { get; set; }
+        public Guid MainLocation { get; set; }
         private static Random rnd = new Random();
 
         public HealingDrone(Guid mainLocationOfDrone) : base("Drone", 20)
@@ -19,11 +19,12 @@ namespace DreamsAndWhatTheyMean.DragonKittyStrangeCharacters
             CounterAttackChance = 10;
             MaxAttack = 10;
             MaxHeal = 500;
+            MainLocation = mainLocationOfDrone;
         }
 
         public override void Turn()
         {
-            var characterList = GameState.CurrentGameState.GetCharactersInLocation(MainLocation.TrackingId, true);
+            var characterList = GameState.CurrentGameState.GetCharactersInLocation(MainLocation, true);
             int characterAmount = characterList.Count;
             int characterToHealIndex = rnd.Next(0, characterAmount);
             var characterToHeal = characterList[characterToHealIndex];
