0b89d928891939cac35de61faa590d896106f299
Materia-xx <170386+Materia-xx@users.noreply.github.com>
2019-05-05 18:02:34 -0700

    Add specialized console writes for perspective of location or character (#99)
    * Switch to tracking characters by a tracking id instead of by name
    
    * Add specialized console writes for perspective of location or character

--- ExampleGame/ExampleCharacters.cs
+++ ExampleGame/ExampleCharacters.cs
@@ -14,8 +14,8 @@ namespace ExampleGame
         public void NewGame(ExampleGameSourceData gameData)
         {
             Player = GameState.CurrentGameState.AddCharacter(new PlayerCharacter("Sally", 50) { MaxAttack = 40, CounterAttackChance = 75 }, gameData.EgLocations.Start);
-            Rat1 = GameState.CurrentGameState.AddCharacter(new Rat("Rat", 7, 23) { MaxAttack = 10, CounterAttackChance = 15 }, gameData.EgLocations.Start);
-            Rat2 = GameState.CurrentGameState.AddCharacter(new Rat("Rat", 8, 15) { MaxAttack = 12, CounterAttackChance = 17 }, gameData.EgLocations.BanquetHall);
+            Rat1 = GameState.CurrentGameState.AddCharacter(new Rat("Joe the rat", 7, 23) { MaxAttack = 10, CounterAttackChance = 15 }, gameData.EgLocations.Start);
+            Rat2 = GameState.CurrentGameState.AddCharacter(new Rat("Henry the rat", 8, 15) { MaxAttack = 12, CounterAttackChance = 17 }, gameData.EgLocations.BanquetHall);
         }
     }
 }
