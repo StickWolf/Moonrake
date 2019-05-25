4ac91a954b1d4b344f40248eee9863ae11db26ac
Materia-xx <170386+Materia-xx@users.noreply.github.com>
2019-05-25 07:21:20 -0700

    Add ability for the client to request creating an account WIP (#125)
    * Switch to tracking characters by a tracking id instead of by name
    
    * Add ability for the client to request creating an account WIP

---
+++ GameClient/Commands/CreateAccountCommand.cs
@@ -0,0 +1,17 @@
+﻿using BaseClientServerDtos.ToClient;
+using NetworkUtils;
+using System.Collections.Generic;
+
+namespace GameClient.Commands
+{
+    public class CreateAccountCommand : IClientCommand
+    {
+        public List<string> ActivatingWords => new List<string>() { "createaccount" };
+
+        public void Execute(List<string> extraWords)
+        {
+            var createDialog = new CreateAccountDialog();
+            createDialog.ShowDialog();
+        }
+    }
+}
