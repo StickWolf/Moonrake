c3c0970a107f5c1f872a56a577f5c2b230445d4a
Matthew Benedict <matt@mattandwell.net>
2019-05-25 07:15:31 -0700

    Add ability for the client to request creating an account WIP

---
+++ GameClient/CreateAccountDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BaseClientServerDtos.ToClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameClient
+{
+    /// <summary>
+    /// Interaction logic for CreateAccountDialog.xaml
+    /// </summary>
+    public partial class CreateAccountDialog : Window
+    {
+        public CreateAccountDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Text != txtPassword2.Text)
+            {
+                // TODO: show why we're returning (because the passwords don't match)
+                return;
+            }
+
+            var dto = new CreateAccountDto();
+            dto.UserName = txtUserName.Text;
+            dto.Password = txtPassword.Text; // TODO: hash/encrypt
+            ServerConnection.SendDtoMessage(dto);
+        }
+    }
+}
