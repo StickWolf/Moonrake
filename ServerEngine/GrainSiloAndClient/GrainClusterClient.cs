2ea1a23faf86055cc77bcc42128e0e27cf818b83
Matthew Benedict <matt@mattandwell.net>
2019-06-09 15:38:09 -0700

    Adding Orleans nugets and adding a basic silo and cluster client with demo code.

---
+++ ServerEngine/GrainSiloAndClient/GrainClusterClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using ServerEngine.GrainInterfaces;
+using ServerEngine.Grains;
+using System.Threading.Tasks;
+
+namespace ServerEngine.GrainSiloAndClient
+{
+    public static class GrainClusterClient
+    {
+        public static IClusterClient ClusterClient { get; set; }
+
+        public static async Task StartAsync()
+        {
+            if (ClusterClient == null)
+            {
+                ClusterClient = new ClientBuilder()
+                    .UseLocalhostClustering()
+                    .Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "GameState";
+                        options.ServiceId = "ServerEngine";
+                    })
+                    .ConfigureApplicationParts(parts =>
+                    {
+                        parts.AddApplicationPart(typeof(IItemGrain).Assembly).WithReferences();
+                    })
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.AddConsole();
+                    })
+                    .Build();
+            }
+            await ClusterClient.Connect();
+        }
+
+        public static async Task StopAsync()
+        {
+            if (ClusterClient != null)
+            {
+                await ClusterClient.Close();
+            }
+        }
+    }
+}
